cd8336ce7513973a98533545be4a653ca72c6b3f
angle4cor <angle4cor@gmail.com>
2023-03-21 09:53:22 +0100

    Bonjour

--- src/be/vdab/hello/HelloGit.java
+++ src/be/vdab/hello/HelloGit.java
@@ -3,5 +3,6 @@ package be.vdab.hello;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Bonjour Git!");
     }
 }
